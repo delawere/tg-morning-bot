e9754adde7c73832bc02a2db6c1a307092580857
Zhiganov Dmitriy <delawere2008@gmail.com>
2020-05-31 22:16:44 +0300

    feat: add currency

--- bot/bot.py
+++ bot/bot.py
@@ -5,9 +5,9 @@ from datetime import time, tzinfo, timezone, datetime
 from telegram.ext import Updater, CommandHandler, Filters
 from config import TG_TOKEN, PROXY, WEATHER_APP_ID, CITY
 
-class User: 
+class User:
   @staticmethod
-  def get_username(user):      
+  def get_username(user):
       if user:
         name = user.first_name
       else:
@@ -43,14 +43,40 @@ class Weather:
         print("Exception (find):", e)
         pass
 
+class Rates:
+  @staticmethod
+  def get_all_rates(currency="EUR"):
+    try:
+      res = requests.get(
+        "https://api.exchangeratesapi.io/latest",
+        params={'base': currency}
+      )
+
+      data = res.json()
+      return data['rates']
+    except Exception as e:
+      print("Exception (find):", e)
+      pass
+
+  @staticmethod
+  def get_rate(fromCurrency = "EUR", toCurrency = "RUB"):
+    rates = Rates().get_all_rates(currency=fromCurrency)
+
+    print(rates)
+    return rates[toCurrency]
+
 def message_handler(bot: Bot, job):
     username = User().get_username(job.context['username'])
     weather = Weather().get_weather()
+    rates = {
+      "USD": round(Rates().get_rate(fromCurrency="USD", toCurrency="RUB")),
+      "EUR": round(Rates().get_rate(fromCurrency="EUR", toCurrency="RUB"))
+    }
 
     desc = weather['desc']
     temp = round(weather['temp'])
     feels_like = round(weather['feels_like'])
-    reply_text = f"*Привет {username}* 👋\n\nСейчас в Москве {desc} ⛅\nТемпература воздуха {temp}°C\nОщущается как {feels_like}°C\nХорошего дня 🙂"
+    reply_text = f"*Привет {username}* 👋\n\nСейчас в Москве {desc} ⛅\nТемпература воздуха {temp}°C\nОщущается как {feels_like}°C\n\n🇺🇸 ${rates['USD']} RUB \n🇪🇺 €{rates['EUR']} RUB\n\nХорошего дня 🙂"
 
     bot.send_message(
       chat_id = job.context['chat_id'],
@@ -66,7 +92,7 @@ def callback_timer(bot, update, job_queue):
       'chat_id': update.message.chat_id,
       'username': update.effective_user
     }
-    notify_time = time(8, 30, 0, 0, tzinfo=d_aware.tzinfo)
+    notify_time = time(9, 0, 0, 0, tzinfo=d_aware.tzinfo)
 
     bot.send_message(chat_id=update.message.chat_id, text='Starting!')
     job_queue.run_daily(message_handler, notify_time, context=context)
@@ -78,11 +104,11 @@ def stop_timer(bot, update, job_queue):
 def main():
     bot = Bot(
       token=TG_TOKEN,
-      base_url=PROXY,
+      base_url=PROXY
     )
     updater = Updater(
       bot=bot,
-    )  
+    )
     updater.dispatcher.add_handler(CommandHandler('start', callback_timer, pass_job_queue=True))
     updater.dispatcher.add_handler(CommandHandler('stop', stop_timer, pass_job_queue=True))
 
